ccf2971e43f86824345b9e7e7cc9f35c713120ba
Arturo Sosa <61207851+arturo-sosa@users.noreply.github.com>
2022-10-12 19:10:03 -0600

    Euler problem 1

---
+++ src/euler/1.ts
@@ -0,0 +1,39 @@
+// https://projecteuler.net/minimal=1
+import {FunctionMetrics, getReport} from "../utils/report";
+
+/**
+ * Find the sum of all numbers below limit that divisible by divisors
+ * @param divisors {Array<number>} array of divisor numbers used to check against numbers
+ * @param limit {number} max number not inclusive to be checked against divisors
+ * @returns {number} the sum of numbers from 0 to limit that are divisible by divisors
+ */
+const getDivisibleNumbersSum = (divisors: Array<number>, limit: number) => {
+    const divisibleNumbers: Array<number> = [];
+
+    for (let current = 1; current < limit; current++) {
+        const isDivisible = divisors.some(div => current % div === 0);
+
+        if (isDivisible)
+            divisibleNumbers.push(current);
+    }
+
+    return divisibleNumbers.reduce((acc, value) => acc + value, 0);
+};
+
+(async () => {
+    const report = await getReport([
+        {
+            fn: (divisors: Array<number>, limit: number) => getDivisibleNumbersSum(divisors, limit),
+            input: [[3, 5], 10],
+            expected: 23,
+        },
+        {
+            fn: (divisors: Array<number>, limit: number) => getDivisibleNumbersSum(divisors, limit),
+            input: [[3, 5], 1000],
+            expected: "???",
+        },
+    ]);
+
+    console.log(report);
+    console.log("Problem solution:", (report[1] as FunctionMetrics).fn.output);
+})();
