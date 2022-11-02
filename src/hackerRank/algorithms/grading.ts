c3c71d3d78ed3de77fb7e43ca3ed60f803f21bfc
Arturo Sosa <61207851+arturo-sosa@users.noreply.github.com>
2022-11-02 10:19:46 -0600

    HackerRank grading documentation

--- src/hackerRank/algorithms/grading.ts
+++ src/hackerRank/algorithms/grading.ts
@@ -3,6 +3,14 @@
 import {getReport} from "../../utils/report";
 import {getReportResults} from "../../utils/report/report";
 
+/**
+ * Returns the grades adjusted with the following rules:
+ *  1) If the grade difference with the next multiple of 5 is less than 3, round up to the next multiple of 5 (I.E. 84 rounds to 85)
+ *  2) If the grade difference with the next multiple of 5 is equal or greater than 3, do not round up (I.E. 87 remains 87)
+ *  3) If the grade is less than 38, do not round up (I.E. 37 remains 37)
+ * @param grades {Array<number>} array of grades to adjust
+ * @returns {Array<number>} array of adjusted grades
+ */
 const gradingStudents = (grades: Array<number>): Array<number> => {
   return grades.map(grade => {
     if (grade < 38) return grade;
